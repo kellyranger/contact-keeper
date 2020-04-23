e31f02656c141c351e4ece85d670131369a5411d
Kelly Ranger <kelly@losgatos-massage.com>
2020-04-22 17:05:39 -0700

    Error at 9:15 in lesson 68 - Cannot read porperty 'msg' of undefined

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -12,35 +12,37 @@ const Contacts = () => {
 
   useEffect(() => {
     getContacts();
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
-  if (contacts.length === 0) {
+  if (contacts !== null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>
   }
 
   return (
     <Fragment>
-      {contacts} !== null && !loading ? (
-      <TransitionGroup>
-        {filtered !== null
-          ? filtered.map(contact => (
-            <CSSTransition
-              key={contact._id}
-              timeout={500}
-              classNames="item">
-              <ContactItem contact={contact} />
-            </CSSTransition>
-          ))
-          : contacts.map(contact => (
-            <CSSTransition
-              key={contact._id}
-              timeout={500}
-              classNames="item">
-              <ContactItem contact={contact} />
-            </CSSTransition>
-          ))}
-      </TransitionGroup>) : <Spinner />
+      {contacts !== null && !loading ? (
+        <TransitionGroup>>
+          {filtered !== null
+            ? filtered.map(contact => (
+              <CSSTransition
+                key={contact._id}
+                timeout={500}
+                classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+            : contacts.map(contact => (
+              <CSSTransition
+                key={contact._id}
+                timeout={500}
+                classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      ) : (
+          <Spinner />)}
     </Fragment>
   );
 };
